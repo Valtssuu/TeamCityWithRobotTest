b9c45459ba22d00fb57a68a6195f894d2679f495
Valtssuu <44770425+Valtssuu@users.noreply.github.com>
2021-06-11 12:57:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-pip3 install robotframework==3.1.2
-pip3 install robotframework-selenium2library==3.0.0
-pip3 install robotframework-seleniumlibrary==4.1.0
-pip3 install RESTinstance==1.0.2
-pip3 install diskcache==4.1.0
 
 robot .
