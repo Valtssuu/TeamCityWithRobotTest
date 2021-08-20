5e4e6b559e66be5711542ddab01fcd2926d32e40
Valtssuu <valtterijniemela@gmail.com>
2021-08-20 12:58:37 +0300

    Update run.sh

--- tests/robot/run.sh
+++ tests/robot/run.sh
@@ -1,10 +1,10 @@
-robot --output original.xml .
+robot --output output.xml .
 
 if [ $? -eq 0 ]; then
 	echo "Everything ok on the first try!"
 	exit 0	
 fi
 
-robot --outputdir output --nostatusrc --rerunfailed output/output.xml --output rerun.xml
+robot --outputdir output --nostatusrc --rerunfailed output.xml --output rerun.xml
 
 
