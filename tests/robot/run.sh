8ac8f31e569a20e225e1f5a58d68e47c1fa2682e
Valtssuu <valtterijniemela@gmail.com>
2021-08-20 15:09:32 +0300

    Update run.sh

--- tests/robot/run.sh
+++ tests/robot/run.sh
@@ -1,9 +1,9 @@
-robot --output original.xml open_chrome.robot 
+robot --output original.xml flakytests.robot 
 
 if [ $? -eq 0 ]; then
 	echo "Everything ok on the first try!"
 	exit 0	
 fi
 
-robot --rerunfailed original.xml --output rerun.xml open_chrome.robot
+robot --rerunfailed original.xml --output rerun.xml flakytests.robot
 rebot --merge original.xml rerun.xml
