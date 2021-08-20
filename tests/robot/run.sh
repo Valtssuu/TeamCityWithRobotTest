c1f2f7a861ff4132a3b0a40676dd476d3e4e95f4
Valtssuu <valtterijniemela@gmail.com>
2021-08-20 11:36:59 +0300

    Update run.sh

--- tests/robot/run.sh
+++ tests/robot/run.sh
@@ -3,4 +3,13 @@ DIR=$(dirname "$SCRIPT")
 REPORTS_DIR=${REPORTS_DIR:-${DIR:-"."}}
 
 pip3 install -r $DIR/required_packages.txt 1>/dev/null
-robot .
+robot --output original.xml .
+
+if [ $? -eq 0 ]; then
+	echo "Everything ok on the first try!"
+	exit 0	
+fi
+
+robot --outputdir output --nostatusrc --rerunfailed output/output.xml --output rerun.xml
+
+
